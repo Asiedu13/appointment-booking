d77b5a18cab071fa8aa812c7fcad7b5df6b7809c
prince <princekofasiedu@gmail.com>
2023-06-28 14:18:10 +0000

    Major UI alignment fixes

--- components/input.js
+++ components/input.js
@@ -3,7 +3,7 @@ export default function textInput(props) {
   return (
     <div className="flex flex-col ">
       <label className="text-sm">{props.name}</label>
-      <div className="flex border-2 border-solid border-[#CCCFCE66] my-2 rounded-lg  w-[360px]">
+      <div className=" w-[95%] flex border-2 border-solid border-[#CCCFCE66] my-2 rounded-lg md:w-[98%] ">
         <input
           className=" flex focus:ring-blue-500 focus:border-blue-500 block w-[360px] h-[40px] p-3  leading-tight border-gray-300 focus:outline-none text-gray-400 pr-16 font-mono js-password"
           type={defType}
