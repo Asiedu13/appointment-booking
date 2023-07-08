8a049c2bf3048c5367510e8555f3c264327924be
prince <princekofasiedu@gmail.com>
2023-07-08 17:33:28 +0000

    Google OAuth authentication complete

--- app/components/button.js
+++ app/components/button.js
@@ -1,6 +1,6 @@
 export default function Button(props) {
   return (
-    <button
+    <button onClick={props.clickHandler}
       className={`w-[98%] flex justify-center items-center border-2 border-solid border-[#CCCFCE66] bg[#fff] p-3 mb-[20px] h-[40px] text-[14px] rounded text-[#808785] font-medium  md:w-[99%]`}
     >
       {props.children}
--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,19 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Provider from "./components/Provider";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Appointment-booking',
-  description: 'Made with love in Ghana',
-}
+  title: "Appointment-booking",
+  description: "Made with love in Ghana",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
-  )
+  );
 }
